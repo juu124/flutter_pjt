b09fafac53af2d32c198b2d5a3406dd59aae1bba
juu124 <juu12424@gmail.com>
2026-04-27 16:46:56 +0900

    1. 화면 전환 라우터 추가

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,4 +1,6 @@
-// 화면 단위 위젯의 이름을 상수 변수로
+//화면 단위 위젯의 이름을 상수 변수로..
 class AppRoutes {
-  static const String home = "/";   // 첫 화면
+  static const String home = "/";
+  static const String about = "/about";
+  static const String myInfo = "/my-info";
 }
